462fcbaa59bf3161c703aa011c1ac8d54a9484e3
Goodwin999 <Vyacheslav.tsitsar@mail.ru>
2023-08-23 21:03:03 +0300

    Сделал ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("HW2");
+        System.out.println("Home Work");
         {
             var dog = 8.0;
             System.out.println(dog);
