0ac1034a51b8ebab40f7183fdbe07631cfda8a85
Goodwin999 <Vyacheslav.tsitsar@mail.ru>
2023-08-23 16:25:01 +0300

    фиксирую

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("HW2");
+        {
+        var dog = 8.0;
+
+            System.out.println(dog);
+
+            var cat = 3.6;
+            System.out.println(cat);
+
+            var paper = 763789;
+            System.out.println(paper);
+
+            var result1 = dog + 4;
+
+            System.out.println("dog + 4 = " + result1);
+
+            var result2 = cat + 4;
+
+            System.out.println("cat + 4 = " + result2);
+
+            var result3 = paper + 4;
+            System.out.println("paper + 4 = " + result3);
+
+            var result4 = dog - 3.5;
+            System.out.println("dog - 3.5 = " + result4);
+        }
+    }
+}
