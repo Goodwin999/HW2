78aa4453ed43f3cf239a799d900b8e1169ab549a
Goodwin999 <vyacheslav.tsitsar@mail.ru>
2023-08-23 18:30:31 +0300

    фиксирую

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("HW2");
         {
-        var dog = 8.0;
-
+            var dog = 8.0;
             System.out.println(dog);
 
             var cat = 3.6;
@@ -25,6 +24,64 @@ public class Main {
 
             var result4 = dog - 3.5;
             System.out.println("dog - 3.5 = " + result4);
+
+            var result5 = cat - 1.6;
+            System.out.println("cat - 1.6 =" + result5);
+
+            var result6 = paper - 7639;
+            System.out.println("paper - 7639 = " + result6);
+
+            var friend = 19;
+            System.out.println("friend");
+
+            var result7 = friend + 2;
+            System.out.println("friend + 2 = " + result7);
+
+            var result8 = friend / 7;
+            System.out.println("friend / 7 = " + result8);
+
+            var frog = 3.5;
+            System.out.println("frog");
+
+            var result9 = frog * 10;
+            System.out.println("frog * 10 = " + result9);
+
+            var result10 = frog / 3.5;
+            System.out.println("frog / 3.5 = " + result10);
+
+            var result11 = frog + 4;
+            System.out.println("frog + 4 = " + result11);
+
+            var mohammedAli = 78.2;
+            var mikeTyson = 82.7;
+            var result12 = mohammedAli + mikeTyson;
+            System.out.println("mohammedAli + mikeTyson = " + result12);
+
+            var result13 = mikeTyson-mohammedAli;
+            System.out.println("mikeTyson - mohammedAli = " + result13);
+
+            var athlete1 = 95.5;
+            var athlete2 = 91.7;
+            var result14 = athlete1 - athlete2;
+            System.out.println("athlete1 - athlete2 = " + result14);
+
+            var result15 = athlete1 % athlete2;
+            System.out.println("разница масс составляет" + "athlete1 % athlete2 = " + result15);
+
+            var watch = 640;
+            var workingDay = 8;
+            var result16 = watch / workingDay;
+            System.out.println("Всего работников в компании - " + "watch / workingDay = " + result16 + "человек");
+
+            var worker = 174;
+            System.out.println("worker");
+
+            var result17 = watch / worker;
+            System.out.println("Если в компании работает 174 человека, то всего по" + "watch / worker = " + result17 + "часа может быть поделено между работниками");
+
+
+
         }
     }
 }
+
